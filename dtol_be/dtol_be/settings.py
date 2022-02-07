ed7f0c40156b803830e885ea017a1f30fa4636ce
Alexey Sokolov <alexey@ebi.ac.uk>
2022-02-07 13:42:04 +0000

    Fixing Django secret issue

--- dtol_be/dtol_be/settings.py
+++ dtol_be/dtol_be/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j7)%40=vzq1=wd-$7o)!fn0hn-3eyuu895esaiuq2gg%no7^8w'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'be.apps.BeConfig'
 ]
 
 MIDDLEWARE = [
