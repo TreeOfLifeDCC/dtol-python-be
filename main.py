e4de75682cb371f27c53e8cf6ae3ef478bb55b01
Alexey Sokolov <alexey@ebi.ac.uk>
2022-02-10 15:27:19 +0000

    Updating details page route

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from elasticsearch import AsyncElasticsearch
 from fastapi import FastAPI
 
@@ -9,13 +10,17 @@ es = AsyncElasticsearch(["localhost:9200"])
 
 @app.get("/{index}")
 async def root(index: str, offset: int = 0, limit: int = 15,
-               sort: str = "rank:desc", filter: str | None = None):
+               sort: str = "rank:desc", filter: str | None = None,
+               search: str | None = None):
+    # data structure for ES query
     body = dict()
+    # building aggregations for every request
     body["aggs"] = dict()
     for aggregation_field in DATA_PORTAL_AGGREGATIONS:
         body["aggs"][aggregation_field] = {
             "terms": {"field": aggregation_field}
         }
+    # adding filters, format: filter_name1:filter_value1, etc...
     if filter:
         filters = filter.split(",")
         body["query"] = {"bool": {"filter": []}}
@@ -24,6 +29,26 @@ async def root(index: str, offset: int = 0, limit: int = 15,
             body["query"]["bool"]["filter"].append(
                 {"term": {filter_name: filter_value}}
             )
+
+    # adding search string
+    if search:
+        # body already has filter parameters
+        if "query" in body:
+            # body["query"]["bool"].update({"should": []})
+            body["query"]["bool"].update({"must": {}})
+        else:
+            # body["query"] = {"bool": {"should": []}}
+            body["query"] = {"bool": {"must": {}}}
+        body["query"]["bool"]["must"] = {"bool": {"should": []}}
+        body["query"]["bool"]["must"]["bool"]["should"].append(
+            {"wildcard": {"organism": {"value": f"*{search}*",
+                                       "case_insensitive": True}}}
+        )
+        body["query"]["bool"]["must"]["bool"]["should"].append(
+            {"wildcard": {"commonName": {"value": f"*{search}*",
+                                         "case_insensitive": True}}}
+        )
+
     response = await es.search(
         index=index, sort=sort, from_=offset, size=limit, body=body
     )
@@ -36,7 +61,8 @@ async def root(index: str, offset: int = 0, limit: int = 15,
 
 @app.get("/{index}/{record_id}")
 async def details(index: str, record_id: str):
+    response = await es.search(index=index, q=f'_id:{record_id}')
     data = dict()
-    data['index'] = index
-    data['record_id'] = record_id
+    data['count'] = response['hits']['total']['value']
+    data['results'] = response['hits']['hits']
     return data
