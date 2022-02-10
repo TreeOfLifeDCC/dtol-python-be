84d51861356fc43fb1d2455229560d99b2098165
Alexey Sokolov <alexey@ebi.ac.uk>
2022-02-10 11:44:37 +0000

    Adding filtering

--- main.py
+++ main.py
@@ -9,11 +9,21 @@ es = AsyncElasticsearch(["localhost:9200"])
 
 @app.get("/{index}")
 async def root(index: str, offset: int = 0, limit: int = 15,
-               sort: str = "rank:desc"):
+               sort: str = "rank:desc", filter: str | None = None):
     body = dict()
     body["aggs"] = dict()
     for aggregation_field in DATA_PORTAL_AGGREGATIONS:
-        body["aggs"][aggregation_field] = {"terms": {"field": aggregation_field}}
+        body["aggs"][aggregation_field] = {
+            "terms": {"field": aggregation_field}
+        }
+    if filter:
+        filters = filter.split(",")
+        body["query"] = {"bool": {"filter": []}}
+        for filter_item in filters:
+            filter_name, filter_value = filter_item.split(":")
+            body["query"]["bool"]["filter"].append(
+                {"term": {filter_name: filter_value}}
+            )
     response = await es.search(
         index=index, sort=sort, from_=offset, size=limit, body=body
     )
